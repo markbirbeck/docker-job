16f5c621bf165e2dad9d6b6d4607a4c223d67eef
Mark Birbeck <mark.birbeck@gmail.com>
2018-06-11 13:45:54 +0100

    [#1] Add name parameter

--- test/unit/cli.js
+++ test/unit/cli.js
@@ -8,6 +8,7 @@ const commandLineArgs = require('command-line-args')
 
 const optionDefinitions = [
   { name: 'detach', alias: 'd', type: Boolean },
+  { name: 'name', type: String },
   { name: 'replicas', type: Number, defaultValue: 1 },
   { name: 'showlogs', type: Boolean },
   { name: 'image', type: String, defaultOption: true }
