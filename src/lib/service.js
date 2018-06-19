c1a545200540a6db3224d388970e39491c1fec42
Mark Birbeck <mark.birbeck@gmail.com>
2018-06-19 09:02:45 +0100

    [#17] Add CLI options to set docker-engine for tunneling

--- src/lib/service.js
+++ src/lib/service.js
@@ -20,9 +20,43 @@
 const dockerEngine = require('docker-engine');
 
 (async () => {
-  const client = await dockerEngine()
   const { poll } = require('./poll');
   const options = require('./option-definitions')()
+  let config
+  if (options.sshHostname) {
+    /**
+     * Get the hostname and possibly a user name
+     */
+
+    let [ user, hostname ] = options.sshHostname.split('@')
+    if (!hostname) {
+      hostname = user
+      user = undefined
+    }
+    config = {
+      username: user,
+      host: hostname
+    }
+
+    /**
+     * If their is a private key then read it and add it to the config:
+     */
+
+    if (options.sshIdentityFile) {
+      config.privateKey = require('fs').readFileSync(__dirname + '/' + options.sshIdentityFile, 'utf8')
+    }
+
+    /**
+     * If the remote connection is via a socket then specify it:
+     */
+
+    if (options.sshRemote) {
+      if (options.sshRemote[0] === '/') {
+        config.socketPath = options.sshRemote
+      }
+    }
+  }
+  const client = await dockerEngine(config)
 
   /**
    * Get info about a service:
