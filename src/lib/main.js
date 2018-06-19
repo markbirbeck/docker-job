6169f792c2f01c7a9da6df3a57df3bc8e37bb337
Mark Birbeck <mark.birbeck@gmail.com>
2018-06-19 16:25:37 +0100

    [#14] Support repeat until by repeatedly running
    Note that this solution creates a new service each time, which we actually don't need.

--- src/lib/main.js
+++ src/lib/main.js
@@ -4,39 +4,41 @@ const main = async (options, config) => {
   const serviceClient = await new ServiceClient.Builder(config).build()
   let shouldRepeat = false
 
-  const id = await serviceClient.run(options.image, options.replicas, options.name)
-  console.log(id)
+  do {
+    const id = await serviceClient.run(options.image, options.replicas, options.name)
+    console.log(id)
 
-  if (!options.detach) {
-    await serviceClient.poll(id, async task => {
-      /**
-       * Once completed get the task's details and use them to get the logs:
-       */
+    if (!options.detach) {
+      await serviceClient.poll(id, async task => {
+        /**
+         * Once completed get the task's details and use them to get the logs:
+         */
 
-      let logs
-      if (options.showlogs || options.repeatUntil) {
-        const state = await serviceClient.inspectTask(task.ID)
-        logs = await serviceClient.logsContainer(state.Status.ContainerStatus.ContainerID)
-      }
-
-      if (options.showLogs) {
-        console.log(logs)
-      }
-
-      /**
-       * If the task is to be repeated until a certain string is found then
-       * check for that in the logs:
-       */
-
-      if (options.repeatUntil) {
-        const re = new RegExp(options.repeatUntil)
-        const m = logs.match(re)
-        if (!m) {
-          shouldRepeat = true
+        let logs
+        if (options.showlogs || options.repeatUntil) {
+          const state = await serviceClient.inspectTask(task.ID)
+          logs = await serviceClient.logsContainer(state.Status.ContainerStatus.ContainerID)
         }
-      }
-    })
-  }
+
+        if (options.showlogs) {
+          console.log(logs)
+        }
+
+        /**
+         * If the task is to be repeated until a certain string is found then
+         * check for that in the logs:
+         */
+
+        if (options.repeatUntil) {
+          const re = new RegExp(options.repeatUntil)
+          const m = logs.match(re)
+          if (!m) {
+            shouldRepeat = true
+          }
+        }
+      })
+    }
+  } while (shouldRepeat)
 }
 
 module.exports = main
