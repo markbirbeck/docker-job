0775d54e042d204c918fc0a00c662f7f4f31590c
Mark Birbeck <mark.birbeck@gmail.com>
2018-06-21 19:55:26 +0100

    [#25] Align CLI test with new args option being returned

--- test/unit/cli.js
+++ test/unit/cli.js
@@ -11,6 +11,7 @@ tap.test('cli', t => {
   t.test('simple example with basic parameters', t => {
     const config = options('--replicas 7 hello-world'.split(' '))
     t.same(config, {
+      args: [],
       replicas: 7,
       image: 'hello-world'
     })
@@ -20,6 +21,7 @@ tap.test('cli', t => {
   t.test('default value for replicas', t => {
     const config = options(['hello-world'])
     t.same(config, {
+      args: [],
       replicas: 1,
       image: 'hello-world'
     })
@@ -29,6 +31,7 @@ tap.test('cli', t => {
   t.test('detach', t => {
     const config = options('--detach hello-world'.split(' '))
     t.same(config, {
+      args: [],
       replicas: 1,
       detach: true,
       image: 'hello-world'
@@ -39,6 +42,7 @@ tap.test('cli', t => {
   t.test('logs', t => {
     const config = options('--showlogs hello-world'.split(' '))
     t.same(config, {
+      args: [],
       replicas: 1,
       showlogs: true,
       image: 'hello-world'
@@ -49,6 +53,7 @@ tap.test('cli', t => {
   t.test('name', t => {
     const config = options('--name myservice hello-world'.split(' '))
     t.same(config, {
+      args: [],
       replicas: 1,
       name: 'myservice',
       image: 'hello-world'
@@ -73,6 +78,7 @@ tap.test('cli', t => {
       .split(' '))
     t.same(config, {
       image: 'hello-world',
+      args: [],
       replicas: 1,
       sshHostname: 'docker@amazonaws.com',
       sshIdentityFile: './docker-swarm.pem',
